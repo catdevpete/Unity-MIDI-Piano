cb6a30d7d90bdc96ba2fb571c6dfb6b9c26f22d7
CatDevPete (Peter Liang) <catdevpete@peter-liang.com>
2021-05-15 11:01:45 +1000

    Finger sizer added to make it easier to press keys by increasing the radius of the capsule colliders on the oculus quest fingers.

---
+++ Assets/Scripts/Oculus/OVRFingerCapsuleSizer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRFingerCapsuleSizer : MonoBehaviour
+{
+	public bool updateRadiusOnTick;
+	public float fingerRadius = 0.01f;
+
+	private OVRSkeleton skeleton;
+	private IList<OVRBoneCapsule> handBoneCapsules;
+	private bool capsulesInit;
+	
+	// Start is called before the first frame update
+    IEnumerator Start()
+    {
+		skeleton = GetComponent<OVRSkeleton>();
+
+		if (skeleton) {
+			while (!skeleton.IsInitialized) {
+				yield return null;
+			}
+
+			handBoneCapsules = skeleton.Capsules;
+
+			foreach (var capsule in handBoneCapsules) {
+				if (!capsule.CapsuleCollider.gameObject.name.Contains("Hand_Start") && !capsule.CapsuleCollider.gameObject.name.Contains("Hand_Thumb1")) {
+					capsule.CapsuleCollider.radius = fingerRadius;
+				}
+			}
+
+			capsulesInit = true;
+		}
+    }
+
+	void Update() {
+		if (!updateRadiusOnTick || !capsulesInit)
+			return;
+		
+		foreach (var capsule in handBoneCapsules) {
+			if (!capsule.CapsuleCollider.gameObject.name.Contains("Hand_Start") && !capsule.CapsuleCollider.gameObject.name.Contains("Hand_Thumb1")) {
+				capsule.CapsuleCollider.radius = fingerRadius;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Piano/FixedPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedPosition : MonoBehaviour
+{
+	private Rigidbody rigidbody;
+	private Vector3 targetPosition;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+		targetPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		rigidbody.position = targetPosition;
+		rigidbody.rotation = Quaternion.AngleAxis();
+	}
+}
